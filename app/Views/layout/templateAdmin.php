dc7ef498904b21925e6e4859b5b39e24caa67d73
PutuPersada <putupersada@gmail.com>
2023-01-24 09:15:49 +0700

    new

--- app/Views/layout/templateAdmin.php
+++ app/Views/layout/templateAdmin.php
@@ -42,7 +42,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
 </head>
 
-<body class="theme-orange">
+<body class="theme-orange" data-spy="scroll" data-target="#navSection">
     <!--================================-->
     <!-- Page Container Start -->
     <!--================================-->
