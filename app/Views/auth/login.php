b2fd2d5534b4bcbee966d99711752e6fce743643
PutuPersada <putupersada@gmail.com>
2023-01-24 12:56:13 +0700

    login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="source/assets/images/favicon.png">
+    <link rel="icon" type="image/png" href="assets/img/smalan.png">
     <title>Login</title>
     <!-- Custom CSS -->
     <link href="source/dist/css/style.min.css" rel="stylesheet">
