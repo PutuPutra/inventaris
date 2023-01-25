e2365507482985d8ef3c4443ae9711a180e4a3cd
PutuPersada <putupersada@gmail.com>
2023-01-25 16:16:30 +0700

    new

--- app/Views/admin/sarana/laptop/fileKomputer.php
+++ app/Views/admin/sarana/laptop/fileKomputer.php
@@ -26,21 +26,25 @@
                             </thead>
                             <tbody>
                                 <?php foreach ($files_komputer as $f) : ?>
-                                    <tr style="text-align: center;">
-                                        <td><?= $f->brand_komputer; ?></td>
-                                        <td><img class="pictures" src="/assets/foto/<?= $f->gambar_komputer; ?>" alt="" width="100px"></img></td>
-                                        <td><?= $f->kondisi_komputer; ?></td>
-                                        <td>
-                                            <?php echo word_limiter($f->spesifikasi_komputer, 2);  ?>
-                                        </td>
-                                        <td><?= $f->jenis_produk_komputer; ?></td>
-                                        <td>
-                                            <div class=" buttons">
-                                                <a href="<?= base_url('editLaptop') . '/' . $f->id ?>" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></i></a>
-                                                <a href="<?= base_url('deleted') . '/' . $f->id ?>" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                <tr style="text-align: center;">
+                                    <td><?= $f->brand_komputer; ?></td>
+                                    <td><img class="pictures" src="/assets/foto/<?= $f->gambar_komputer; ?>" alt=""
+                                            width="100px"></img></td>
+                                    <td><?= $f->kondisi_komputer; ?></td>
+                                    <td>
+                                        <?php echo word_limiter($f->spesifikasi_komputer, 2);  ?>
+                                    </td>
+                                    <td><?= $f->jenis_produk_komputer; ?></td>
+                                    <td>
+                                        <div class=" buttons">
+                                            <a href="<?= base_url('editLaptop') . '/' . $f->id ?>"
+                                                class="btn btn-warning"><i
+                                                    class="fa-solid fa-pen-to-square"></i></i></a>
+                                            <a href="<?= base_url('deleted') . '/' . $f->id ?>"
+                                                class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
+                                        </div>
+                                    </td>
+                                </tr>
                                 <?php
                                 endforeach ?>
                             </tbody>
