d257b426cdc2b1c7d3ad912370991cc50d14a8f6
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2023-02-02 09:28:48 +0700

    membuat model dan controler untuk kelas

---
+++ app/Models/ModelKelas.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelKelas extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'kelas';
+    protected $primaryKey       = 'id_kelas';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $protectFields    = true;
+
+    protected $allowedFields    = [
+        'nama_kelas',
+        'wali_kelas',
+        'ketua_kelas'
+    ];
+}
