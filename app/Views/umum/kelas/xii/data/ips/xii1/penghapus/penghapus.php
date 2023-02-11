d45137e848c36bc7463b0ef206eb9ec18b0873e1
PutuPersada <putupersada@gmail.com>
2023-02-11 23:38:47 +0700

    new

---
+++ app/Views/umum/kelas/xii/data/ips/xii1/penghapus/penghapus.php
@@ -0,0 +1,16 @@
+<?= $this->extend('/layout/templateUmum'); ?>
+<?= $this->section('umum'); ?>
+
+<!-- Page Header Start -->
+<div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container">
+        <h1 class="display-3 mb-4 animated slideInDown">Penghapus Kelas X 1</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
+
+
+<?= $this->endSection(); ?>
