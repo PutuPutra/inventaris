ae7cd317285cd1ff2a88c8c57d57dd53d8bd7b51
PutuPersada <putupersada@gmail.com>
2023-02-12 08:35:28 +0700

    new

---
+++ app/Views/umum/kelas/xii/data/ips/xii1/papantulis/papanTulis.php
@@ -0,0 +1,60 @@
+<?= $this->extend('/layout/templateUmum'); ?>
+<?= $this->section('umum'); ?>
+
+<!-- Page Header Start -->
+<div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container">
+        <h1 class="display-3 mb-4 animated slideInDown">Papan Tulis Kelas XII IPS 1</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
+
+<!-- Start -->
+<div class="container-xxl service py-5">
+    <div class="container">
+        <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <!-- <p class="d-inline-block border rounded text-primary fw-semi-bold py-1 px-3">Sarana</p> -->
+            <!-- <h6 class="display-5 mb-5">Data Buku</h6> -->
+        </div>
+
+
+    </div>
+</div>
+<!-- End -->
+
+<div class="page-heading p-5" style="margin-top:-100px;">
+
+    <section class="section">
+        <div class="card">
+            <!-- <div class="card-header">
+                Simple Datatable
+            </div> -->
+            <div class="card-body">
+                <table class="table table-striped" id="table1">
+                    <thead>
+                        <tr>
+                            <th>Ukuran</th>
+                            <th>Gambar</th>
+                            <th>Kondisi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td id="isiTable">Erlangga</td>
+                            <td id="isiTable"><img src="/assets/foto/ryzen.jpg" alt="" height="100" width="150"></td>
+                            <td id="isiTable">sedang diperbaiki</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </section>
+</div>
+
+
+<?= $this->endSection(); ?>
--- app/Views/umum/kelas/xii/data/mipa/xii6/buku/filebuku.php
+++ app/Views/umum/kelas/xii/data/mipa/xii6/buku/filebuku.php
@@ -1,10 +1,12 @@
 <?= $this->extend('/layout/templateUmum'); ?>
 <?= $this->section('umum'); ?>
 
+
+
 <!-- Page Header Start -->
 <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container">
-        <h1 class="display-3 mb-4 animated slideInDown">Buku Kelas XII IPA 6</h1>
+        <h1 class="display-3 mb-4 animated slideInDown">Buku Kelas XII MIPA 6</h1>
         <nav aria-label="breadcrumb animated slideInDown">
 
         </nav>
@@ -12,5 +14,53 @@
 </div>
 <!-- Page Header End -->
 
+<!-- Start -->
+<div class="container-xxl service py-5">
+    <div class="container">
+        <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <!-- <p class="d-inline-block border rounded text-primary fw-semi-bold py-1 px-3">Sarana</p> -->
+            <!-- <h6 class="display-5 mb-5">Data Buku</h6> -->
+        </div>
 
+
+    </div>
+</div>
+<!-- End -->
+
+<div class="page-heading p-5" style="margin-top:-100px;">
+
+    <section class="section">
+        <div class="card">
+            <!-- <div class="card-header">
+                Simple Datatable
+            </div> -->
+            <div class="card-body">
+                <table class="table table-striped" id="table1">
+                    <thead>
+                        <tr>
+                            <th>Penerbit</th>
+                            <th>Gambar</th>
+                            <th>Deskripsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td id="isiTable">Erlangga</td>
+                            <td id="isiTable"><img src="/assets/foto/ryzen.jpg" alt="" height="100" width="150"></td>
+                            <td id="isiTable">
+                                <div class="textarea">
+                                    <textarea disabled class="container-fluid" name="" id="description" cols="50" rows="5" style="border:none;">
+                                            buku ini sangat bagus
+                                    </textarea>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </section>
+</div>
 <?= $this->endSection(); ?>
