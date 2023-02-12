3e79cb590326b8b556722fb31c61e04a7bec3410
PutuPersada <putupersada@gmail.com>
2023-02-12 08:09:04 +0700

    new

--- app/Views/umum/kelas/xi/data/ips/xi1/papantulis/papanTulis.php
+++ app/Views/umum/kelas/xi/data/ips/xi1/papantulis/papanTulis.php
@@ -4,7 +4,7 @@
 <!-- Page Header Start -->
 <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container">
-        <h1 class="display-3 mb-4 animated slideInDown">Penghapus Kelas XI IPS 2</h1>
+        <h1 class="display-3 mb-4 animated slideInDown">Papan Tulis Kelas XI IPS 1</h1>
         <nav aria-label="breadcrumb animated slideInDown">
 
         </nav>
@@ -36,7 +36,7 @@
                 <table class="table table-striped" id="table1">
                     <thead>
                         <tr>
-                            <th>Merek</th>
+                            <th>Ukuran</th>
                             <th>Gambar</th>
                             <th>Kondisi</th>
                         </tr>
@@ -45,7 +45,7 @@
                         <tr>
                             <td id="isiTable">Erlangga</td>
                             <td id="isiTable"><img src="/assets/foto/ryzen.jpg" alt="" height="100" width="150"></td>
-                            <td id="isiTable">Baik/td>
+                            <td id="isiTable">sedang diperbaiki</td>
                         </tr>
 
                     </tbody>
@@ -55,5 +55,6 @@
 
     </section>
 </div>
+
 
 <?= $this->endSection(); ?>
