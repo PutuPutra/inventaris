bb1cf6a86e8a57416d151064d2692e4172dedbef
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2023-02-16 10:24:25 +0700

    sudah bisa menampilkan prasaraana

--- app/Controllers/Umum.php
+++ app/Controllers/Umum.php
@@ -623,6 +623,7 @@ class Umum extends BaseController
         $data = [
             'navbar1' => null,
             'navbar2' => null,
+            'books' => (new BukuModel())->where('id_kelas', 1)->findAll() 
         ];
         return view('umum/kelas/x/data/x1/buku/filebuku', $data);
     }
@@ -631,6 +632,7 @@ class Umum extends BaseController
         $data = [
             'navbar1' => null,
             'navbar2' => null,
+            'boards' => (new PapanTulisModel())->where('id_kelas', 1)->findAll()
         ];
         return view('umum/kelas/x/data/x1/papantulis/papanTulis', $data);
     }
@@ -699,6 +701,7 @@ class Umum extends BaseController
         $data = [
             'navbar1' => null,
             'navbar2' => null,
+            'books' => (new BukuModel())->where('id_kelas', 2)->findAll(),
         ];
         return view('umum/kelas/x/data/x2/buku/filebuku', $data);
     }
