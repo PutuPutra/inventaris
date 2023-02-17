848d073ba95dc257ec37f7ca66ccc61e20df2b7d
PutuPersada <putupersada@gmail.com>
2023-02-18 06:41:59 +0700

    foto

--- app/Views/umum/kelas/x/data/x1/foto/foto.php
+++ app/Views/umum/kelas/x/data/x1/foto/foto.php
@@ -42,11 +42,15 @@
                         </tr>
                     </thead>
                     <tbody>
+                        <?php foreach ($foto as $f) : ?>
                         <tr>
-                            <td id="isiTable">Erlangga</td>
-                            <td id="isiTable"><img src="/assets/foto/ryzen.jpg" alt="" height="100" width="150"></td>
-                            <td id="isiTable">sedang diperbaiki</td>
+                            <td><?= $f->ukuran_foto ?></td>
+                            <td><img src="/assets/foto/<?= $f->gambar_foto ?>" width="100"></td>
+                            <td>
+                                <?= $f->kondisi_foto ?>
+                            </td>
                         </tr>
+                        <?php endforeach; ?>
 
                     </tbody>
                 </table>
