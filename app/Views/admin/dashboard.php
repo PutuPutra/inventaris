3f7ab5992b6e496d2fa3b6a9581e8906c4bb775e
PutuPersada <putupersada@gmail.com>
2023-04-02 14:23:16 +0700

    Update

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -134,13 +134,12 @@
                         </div>
                     </div>
 
-                    <div class="col-6 col-lg-3 col-md-6">
+                    <!-- <div class="col-6 col-lg-3 col-md-6">
                         <div class="card">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                                         <div class="stats-icon blue mb-2">
-                                            <!-- <i class="iconly-boldBookmark"></i> -->
                                             <i class="fa-regular fa-building"></i>
                                         </div>
                                     </div>
@@ -151,7 +150,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                     <div class="col-6 col-lg-3 col-md-6">
                         <div class="card">
                             <div class="card-body px-4 py-4-5">
