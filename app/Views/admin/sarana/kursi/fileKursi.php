4eb10d00dc6d3f7da5629cce0d62e3b30732014a
PutuPersada <putupersada@gmail.com>
2023-05-08 22:41:52 +0700

    Kursi

--- app/Views/admin/sarana/kursi/fileKursi.php
+++ app/Views/admin/sarana/kursi/fileKursi.php
@@ -18,7 +18,10 @@
                                 <tr>
                                     <th>Ukuran (PxL) cm</th>
                                     <th>Gambar</th>
+                                    <th>Nomor Seri</th>
                                     <th>Kondisi</th>
+                                    <th>Ruangan</th>
+                                    <th>Kelas</th>
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
@@ -26,8 +29,11 @@
                                 <?php foreach ($files_kursi as $f) : ?>
                                     <tr style="text-align: center;">
                                         <td><?= $f->ukuran_kursi; ?></td>
+                                        <td><?= $f->serial_number; ?></td>
                                         <td><img class="pictures" src="/assets/dokumen/kursi/<?= $f->gambar_kursi; ?>" alt="" width="100px"></img></td>
                                         <td><?= $f->kondisi_kursi; ?></td>
+                                        <td><?= $f->nama_ruangan; ?></td>
+                                        <td><?= $f->nama_kelas; ?></td>
                                         <td>
                                             <div class="buttons">
                                                 <a href="<?= base_url('kursi/edit') . '/' . $f->id ?>" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></i></a>
